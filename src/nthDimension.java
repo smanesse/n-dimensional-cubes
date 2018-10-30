8714c4971577f0117f1fc97147d835c9df09de9b
Seth Manesse <scmanesse@gmail.com>
2018-10-30 08:04:29 -0600

    changed to O(N) algorithm

--- src/nthDimension.java
+++ src/nthDimension.java
@@ -17,7 +17,7 @@ public class nthDimension {
                 System.out.println("Please enter positive values.");
             } else if (n < k) {
                 System.out.println("Please enter n and m such that m<n.");
-            } else if (k>30) {
+            } else if (n > 30) {
                 System.out.println("Your numbers were too large.");
             } else {
                 break;
@@ -29,7 +29,7 @@ public class nthDimension {
             result = 1; //edge case
         } else {
             //create array of integers 1 through k.
-            long[] nums = new long[n - k+1];
+            long[] nums = new long[n - k + 1];
             for (int i = 1; i <= nums.length; i++) {
                 nums[i - 1] = i;
             }
@@ -42,6 +42,7 @@ public class nthDimension {
              * 1   4  10  20  35  56
              * 1   5  15  35  50 106
              */
+            /* This is my O(n^2) algorithm.
             int iteration = 0;
             do {
                 long[] nums2 = nums.clone();
@@ -52,9 +53,25 @@ public class nthDimension {
                 nums = nums2;
                 iteration++;
             } while (iteration < k - 1);
+
             result = (long) Math.pow(2, k) * nums[n - k]; //multiply the mth term of the k order set by 2^k
+            */
+
+            //Then I looked up the actual solution, which was O(n), so I decided I should probably use that instead.
+            result = (long) Math.pow(2, k) * factorial(n)/(factorial(n-k)*factorial(k));
         }
         System.out.printf("Number of %d-dimensional squares in %d-dimensional cuboid: %d", (n - k), n, result);
+    }
+
+    static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        long result = 1;
+        for (int i = n; i>1; i--) {
+            result *=i;
+        }
+        return result;
     }
 
 
